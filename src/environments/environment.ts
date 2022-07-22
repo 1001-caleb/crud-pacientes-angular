322145d3f6c29889b84ccf159178f26a195ec271
Caleb Parrra <mccalebyt@gmail.com>
2022-07-22 18:57:16 -0500

    deploy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,10 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
 };
 
-export const API = 'https://nest-api-pacientes.herokuapp.com/'
+export const API = 'https://nest-api-pacientes.herokuapp.com/';
 
 /*
  * For easier debugging in development mode, you can import the following file
